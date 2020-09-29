7a193602623d5b95d7768eb42a11e076694352f7
Saujanya Pandey <saujanya.pandey2000@gmail.com>
2020-09-29 11:36:54 +0530

    remove storage bugs

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const addButton = document.querySelector('.addBtn')
+const editBtn = document.querySelector('container .editBtn')
 var inputBox = document.querySelector('.add-item')
 const container = document.querySelector('.container ul')
+
+// initialise array to store todo-items
+if(window.localStorage.getItem("todos") == undefined){
+    var todos = []
+    window.localStorage.setItem("todos", JSON.stringify(todos))
+}
+
+// convert json to javascript value
+var todosEX = window.localStorage.getItem("todos")
+var todos = JSON.parse(todosEX)
 
 class item{
 	constructor(name){
@@ -31,44 +42,68 @@ class item{
         itemBox.appendChild(editButton)
         itemBox.appendChild(deleteButton)
 
-        editButton.addEventListener('click', () => this.editItem(input))
+        editButton.addEventListener('click', () => this.editItem(input, name))
 
-        deleteButton.addEventListener('click', () => this.deleteItem(itemBox))
+        deleteButton.addEventListener('click', () => this.deleteItem(itemBox, name))
     }
 
-    editItem(input) {
+    // edit button func
+    editItem(input, name) {
         if( input.disabled == true) {
             input.disabled = false
             input.focus()
         }
         else {
+            let indexof = todos.indexOf(name)
             input.disabled = !input.disabled
+            
+            let newVal = input.value
+            todos[indexof] = newVal
+            window.localStorage.setItem("todos", JSON.stringify(todos))
         }
     }
 
-    deleteItem(item) {
+    // delete button func
+    deleteItem(item, name) {
         container.removeChild(item)
+        let index = todos.indexOf(name)
+        todos.splice(index, 1)
+        window.localStorage.setItem("todos", JSON.stringify(todos))
         alert('Item deleted.')
     }
 }
 
+// add item function
 function addItem() {
     if(inputBox.value !== "") {
+        inputBox.value = inputBox.value.trim()
         new item(inputBox.value)
+        todos.push(inputBox.value)
+        window.localStorage.setItem("todos", JSON.stringify(todos))
         inputBox.value = ""
         alert('Item added.')
+        inputBox.focus()
     }
     else {
         alert('Item is blank, please enter data.')
+        inputBox.focus()
     }
 }
 
+// response to add-btn click
 addButton.addEventListener('click', addItem)
 
+// response to enter-btn click
 window.addEventListener('keydown', (e)=> {
     if(e.which == 13) {
         addItem()
     }
 })
 
+// default item 
 new item("Default item (feel free to delete)")
+
+// display all stored items  
+for (var i = 0 ; i < todos.length ; i++){
+    new item(todos[i]);
+}
